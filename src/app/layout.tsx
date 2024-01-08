df4aebcca9ac08efc2ba28731eed7255bf80aa0a
aguspranyoto <agusprnyt@gmail.com>
2024-01-08 18:14:59 +0700

    add sing in with google

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,17 @@
+import Navbar from "@/components/Navbar";
+import { Toaster } from "@/components/ui/Toaster";
+import { cn } from "@/lib/utils";
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 
 export const metadata = {
   title: "Breadit",
   description: "A Reddit clone built with Next.js and TypeScript.",
 };
+
+const inter = Inter({
+  subsets: ["latin"],
+});
 
 export default function RootLayout({
   children,
@@ -11,8 +19,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn(
+        "bg-white text-slate-900 antialiased light",
+        inter.className
+      )}
+    >
+      <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
+        <div className="container mx-auto max-w-7xl h-full pt-12">
+          {children}
+        </div>
+
+        <Toaster />
+      </body>
     </html>
   );
 }
